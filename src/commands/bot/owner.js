afd7bbf03335a8a4c590d0de7e3ea5f5ecca6e9a
Rin <107134739+rinxyzz@users.noreply.github.com>
2022-12-27 02:15:25 +0700

    fix

--- src/commands/bot/owner.js
+++ src/commands/bot/owner.js
@@ -3,16 +3,16 @@ const Discord = require('discord.js');
 module.exports = async (client, interaction, args) => {
     client.embed({
         title: `・Owner information`,
-        desc: `____________________________`,
+        desc: `I'm a software developer in my free time and student living in UK, I like to make android apps, websites, discord bots. I'm good in Html, Css, Kotlin, Java, & JavaScript.`,
         thumbnail: client.user.avatarURL({ dynamic: true, size: 1024 }),
         fields: [{
             name: "<:blue_crown:1012017210992115812> ┆ Owner name",
-            value: `iMiku`,
+            value: `RinSCH`,
             inline: true,
         },
         {
             name: "<:discord:1012017257158824027> ┆ Discord tag",
-            value: `iMiku#4797`,
+            value: `RinSchariacs#4797`,
             inline: true,
         }],
         type: 'editreply'
